87d521f730b4df4e2be646ace88fc1ed09b08909
Nathan <101812777+corndogit@users.noreply.github.com>
2024-01-21 21:21:46 +0000

    wip: audio streaming

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
+import io
+
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
 from pydantic import PositiveInt
 from pathlib import Path
+from ffmpeg.asyncio import FFmpeg
+import wave
 
 from src.quiz.questions import get_random_song, get_song_by_id
 from src.quiz.connection import ConnectionManager
 
+# setup stuff
 app = FastAPI()
 manager = ConnectionManager()
 path = Path(__file__)
@@ -31,12 +37,31 @@ def get_song_by_song_id(song_id: int):
     return song_data
 
 
-@app.websocket("/ws_audio/")
-async def stream_song(websocket: WebSocket):
-    await manager.connect(websocket)
-    try:
-        while True:
-            # receive song id from the websocket and return appropriate audio stream
-            song_id = await websocket.receive_text()
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+# @app.websocket("/ws_audio")
+# async def stream_song(websocket: WebSocket):
+#     await manager.connect(websocket)
+#     try:
+#         while True:
+#             # receive song id from the websocket and return appropriate audio stream
+#             # song_id = await websocket.receive_text()
+#             ffmpeg = (FFmpeg()
+#                       .input("./resources/songs/TECHNOPHOBIA.mp3")
+#                       .output("pipe:1", {"codec:a": "pcm_s16le"}, f='wav', vn=None))
+#             wav_bytes = await ffmpeg.execute()
+#             with wave.open(io.BytesIO(wav_bytes), 'rb') as wave_file:
+#                 manager.send_personal_message()
+#     except WebSocketDisconnect:
+#         manager.disconnect(websocket)
+
+async def audio_stream_generator(file_path: str):
+    ffmpeg = (FFmpeg()
+              .input(file_path)
+              .output("pipe:1", {"codec:a": "pcm_s16le"}, f='wav', vn=None))
+    wave_bytes = await ffmpeg.execute()
+    with wave.open(io.BytesIO(wave_bytes), 'rb') as wave_file:
+        yield wave_file.readframes(wave_file.getnframes())
+
+
+@app.get("/audio")
+async def stream_song():
+    return StreamingResponse(audio_stream_generator("./resources/songs/TECHNOPHOBIA.mp3"))
