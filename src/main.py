d2a266602666b9fa795a4673cdb902787d45799e
Nathan <admin@corndogit.dev>
2024-01-21 11:34:35 +0000

    wip: configure websocket for streaming audio

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
 from pydantic import PositiveInt
+from pathlib import Path
 
 from src.quiz.questions import get_random_song, get_song_by_id
+from src.quiz.connection import ConnectionManager
 
 app = FastAPI()
+manager = ConnectionManager()
+path = Path(__file__)
+songs_dir_path = path / "../resources/songs"
+app.mount("/resources/songs", StaticFiles(directory=songs_dir_path.resolve()), name="songs")
 
 
 @app.get("/")
@@ -22,3 +29,14 @@ def get_song_by_song_id(song_id: int):
     if len(song_data) == 0:
         raise HTTPException(status_code=404, detail="No song found with this ID")
     return song_data
+
+
+@app.websocket("/ws_audio/")
+async def stream_song(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            # receive song id from the websocket and return appropriate audio stream
+            song_id = await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
