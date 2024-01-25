ad0bf3ea87edce9bb43aff3c79d363f19248274e
Nathan <101812777+corndogit@users.noreply.github.com>
2024-01-25 23:31:49 +0000

    add song_id to the song data body

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,7 @@ client = TestClient(app)
 @pytest.fixture
 def song_data_schema():
     schema = {
+        "song_id": 13003,
         "filename": "D.C.fish.mp3",
         "title": "D.C.fish",
         "alt_titles": [
