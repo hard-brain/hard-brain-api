da093fbfc843a9daebf86074f6b442f41404ad4e
Nathan <101812777+corndogit@users.noreply.github.com>
2024-04-11 22:27:58 +0100

    include all iidx songs from 1st style to resident

--- src/quiz/questions.py
+++ src/quiz/questions.py
@@ -5,12 +5,23 @@ from typing import List, Dict
 
 from pydantic.types import PositiveInt
 
+file_path = Path(__file__)
+songs_dir = set([file.stem for file in Path(file_path / "../../resources/songs").resolve().iterdir()])
+
 
 def _load_song_data() -> Dict:
-    file_path = Path(__file__)
-    path = file_path / "../../resources/song_data.json"
-    with open(path.resolve(), 'r', encoding="utf-8") as file:
+    json_path = file_path / "../../resources/song_data.json"
+    with open(json_path.resolve(), 'r', encoding="utf-8") as file:
         return json.load(file)['songs']
+
+
+def _check_song_exists(key: str) -> bool:
+    """
+    Check if there is a file with matching song id using the JSON keys.
+    :param key: JSON key to match against files
+    :return: True if there is a file named {key}.mp3
+    """
+    return key in songs_dir
 
 
 def get_random_song(number_of_songs: PositiveInt) -> List[Dict]:
@@ -20,9 +31,21 @@ def get_random_song(number_of_songs: PositiveInt) -> List[Dict]:
     """
     song_data = _load_song_data()
 
-    def yield_song(limit: PositiveInt):
+    def yield_song(limit: PositiveInt, max_retries=10):
         for i in range(limit):
-            key = random.choice(list(song_data.keys()))
+            key = None
+            retries = 0
+            song_id = None
+
+            while not key and retries < max_retries:
+                song_id = random.choice(list(song_data.keys()))
+                if _check_song_exists(song_id):
+                    key = song_id
+                retries += 1
+
+            if not key:
+                raise FileNotFoundError(f"Did not find a file matching id '{song_id}' after {max_retries} retries")
+
             yield song_data[key]
 
     return [song for song in yield_song(number_of_songs)]
--- src/resources/songs/README.md
+++
@@ -1,2 +0,0 @@
-Songs used for Hard Brain quiz questions are placed here, but not included on the main repo due to file size and legal
-reasons.
